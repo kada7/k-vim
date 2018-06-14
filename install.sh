07d661f33617bb31b99d98d6b35bfe0cfe156531
kada7 <still0509@gmail.com>
2018-06-14 07:10:42 -0700

    add golang

--- install.sh
+++ install.sh
@@ -86,14 +86,14 @@ export SHELL=$system_shell
 echo "Step4: compile YouCompleteMe"
 echo "It will take a long time, just be patient!"
 echo "If error,you need to compile it yourself"
-echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && python install.py --clang-completer"
+echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && python install.py --clang-completer --go-completer"
 cd $CURRENT_DIR/bundle/YouCompleteMe/
 git submodule update --init --recursive
 if [ `which clang` ]   # check system clang
 then
-    python install.py --clang-completer --system-libclang   # use system clang
+    python install.py --clang-completer --go-completer --system-libclang   # use system clang
 else
-    python install.py --clang-completer
+    python install.py --clang-completer --go-completer
 fi
 
 echo "Install Done!"
